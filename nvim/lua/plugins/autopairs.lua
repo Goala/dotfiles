2d9caa32d9fd587518959a7520c380aaf093067e
Goala <max_stenke@yahoo.de>
2026-02-04 22:08:42 +0100

    Fix autopairs

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -2,25 +2,33 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		config = true,
-		-- config = function()
-		-- local autopairs = require("nvim-autopairs") -- import nvim-autopairs
+		config = function()
+			local autopairs = require("nvim-autopairs") -- import nvim-autopairs
 
-		-- setup autopairs
-		-- autopairs.setup({
-		-- 	check_ts = true, -- treesitter enabled
-		-- })
-		-- -- import nvim-autopairs completion functionality
-		-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		-- -- import nvim-cmp plugin (completions plugin)
-		-- local cmp = require("cmp")
-		-- -- make autopairs and completion work together
-		-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		-- end,
+			autopairs.setup({
+				check_ts = true, -- treesitter enabled
+				disable_filetype = { "TelescopePrompt", "guihua", "guihua_rust", "clap_input" },
+				fast_wrap = {},
+			})
+			-- import nvim-autopairs completion functionality
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			-- import nvim-cmp plugin (completions plugin)
+			local cmp = require("cmp")
+			-- make autopairs and completion work together
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
 	},
 	{
 		"windwp/nvim-ts-autotag",
 		event = "InsertEnter",
-		config = true,
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = true,
+				},
+			})
+		end,
 	},
 }
