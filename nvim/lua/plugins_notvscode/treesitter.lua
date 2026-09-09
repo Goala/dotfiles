b0eb664efdafb12c18a555597e7c46729ac7b06f
Goala <max_stenke@yahoo.de>
2026-09-09 09:50:06 +0200

    Update treesitter

--- nvim/lua/plugins_notvscode/treesitter.lua
+++ nvim/lua/plugins_notvscode/treesitter.lua
@@ -2,11 +2,32 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		branch = "main",
-		lazy = false,
+		lazy = true,
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter").setup({
 				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			local parsers = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"markdown",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"json",
+				"bash",
+			}
+			require("nvim-treesitter").install(parsers)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "*",
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
 			})
 
 			-- https://neovim.io/doc/user/fold.html#fold-commands
